22f48e5d258774aac35335e053ab501d91694fdf
Kiirya Arnold <kiiryarnold@gmail.com>
2025-01-12 11:27:56 +0300

    Accounting module

--- modules/accounting/views/dashboard/manage.php
+++ modules/accounting/views/dashboard/manage.php
@@ -10,14 +10,15 @@
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor" class="tw-w-6 tw-h-6 tw-text-neutral-500">
                                 <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12" />
+                                    d="M3.75 3.75h16.5v16.5H3.75V3.75zM8.25 3.75v7.5h7.5v-7.5H8.25zm0 12v4.5h7.5v-4.5H8.25z" />
                             </svg>
 
+
                             <span class="tw-text-neutral-700">
-                                <?php echo _l('home_payment_records'); ?>
+                                Accounting <?php echo new_html_entity_decode($title); ?>
                             </span>
                         </p>
-  				  <h4 class="no-margin text-bold ptop-15"><i class="fa fa-dashboard menu-icon"></i> <?php echo new_html_entity_decode($title); ?></h4>
+  				 
           </div>
 
           <div class="col-md-6">
