46fb781efee6e5caa48ef675847ce02c15725a00
Kiirya Arnold <kiiryarnold@gmail.com>
2025-01-12 11:54:02 +0300

    Accounting module

--- modules/accounting/views/transfer/manage.php
+++ modules/accounting/views/transfer/manage.php
@@ -12,7 +12,7 @@
                   d="M15.75 6.75l3 3-3 3m0-6H8.25a2.25 2.25 0 00-2.25 2.25v6M8.25 17.25l-3-3 3-3m0 6h7.5a2.25 2.25 0 002.25-2.25v-6" />
             </svg>
 
-            <span class="tw-text-neutral-700 tw-text-2xl">
+            <span class="tw-text-neutral-700" style="text-size: 16px">
                <?php echo _l($title); ?>
             </span>
          </p>
