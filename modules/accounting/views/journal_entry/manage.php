e0bf922eee51236ecfcc9121a2940828349e8573
Kiirya Arnold <kiiryarnold@gmail.com>
2025-01-12 11:39:05 +0300

    Accounting module

--- modules/accounting/views/journal_entry/manage.php
+++ modules/accounting/views/journal_entry/manage.php
@@ -19,7 +19,12 @@
          
           <hr />
           <div>
-            <a href="<?php echo admin_url('accounting/new_journal_entry'); ?>" class="btn btn-info mbot15"><?php echo _l('add'); ?></a>
+          <a href="<?php echo admin_url('accounting/new_journal_entry'); ?>"
+                            class="btn btn-primary">
+                            <i class="fa-regular fa-plus tw-mr-1"></i>
+                           New journal entry
+          </a>
+           
           </div>
           <div class="row">
             <div class="col-md-3">
